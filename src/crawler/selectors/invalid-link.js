3986fc4df218f1f8faedfe60926607bff1dea331
Einar Ólafsson <einsiol@Einars-MBP.fritz.box>
2019-10-06 16:37:55 +0200

    fix JSDoc lint warnings

--- src/crawler/selectors/invalid-link.js
+++ src/crawler/selectors/invalid-link.js
@@ -1,9 +1,10 @@
 /**
  * Checks a url link that contains invalid format that are not common urls starting with http://, eg. if they contain hash, mailto or tel:
+ *
  * @memberof module:selectors
  * @function
  * @param {string} link - The url link that will be extracted
- * @return {boolean}
+ * @returns {boolean} - returns true if link is invalid, else false
  */
 const invalidLink = link => Boolean(link.match(/tel:|mailto:|^#/ug))
 
